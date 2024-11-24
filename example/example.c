e2bdf152d0f7775833dac4c4c8ea8f755de03ff6
TuriSc <turiscand@gmail.com>
2024-11-24 21:55:41 +0000

    Change example I2s pins

--- example/example.c
+++ example/example.c
@@ -16,8 +16,8 @@
 
 #elif USE_AUDIO_I2S
   #include "sound_i2s.h"
-  #define I2S_DATA_PIN             9 // -> I2S DIN
-  #define I2S_CLOCK_PIN_BASE       10 // -> I2S BCK
+  #define I2S_DATA_PIN             28 // -> I2S DIN
+  #define I2S_CLOCK_PIN_BASE       26 // -> I2S BCK
   // The third required connection is GPIO 27 -> I2S LRCK (BCK+1)
 
   static const struct sound_i2s_config sound_config = {
